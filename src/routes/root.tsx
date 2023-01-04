3b4bc9e19cbe841fc248ae5d764e696d91205957
Stephen Buley <stephen.a.buley@gmail.com>
2023-01-04 13:53:02 +0100

    Initial Commit, create Root Route, error page, and contacts route

---
+++ src/routes/root.tsx
@@ -0,0 +1,40 @@
+import { Outlet, Link } from "react-router-dom"
+
+export default function Root() {
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+          <form role="search" id="search-form">
+            <input
+              type="search"
+              aria-label="search-contacts"
+              id="q"
+              placeholder="search"
+              name="q"
+            />
+            <div id="search-spinner" aria-hidden hidden={true}></div>
+            <div className="sr-only" aria-live="polite"></div>
+          </form>
+          <form method="post">
+            <button type="submit">New</button>
+          </form>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to={`contacts/1`}>Your Name</Link>
+            </li>
+            <li>
+              <Link to={`contacts/2`}>Your Friend</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </>
+  )
+}
